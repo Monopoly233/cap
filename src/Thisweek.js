0a72e42c5fee5a45f7f473ed7b5c0a903322f35f
Monopoly233 <lslsooke17@gmail.com>
2024-06-08 12:53:32 -0700

    change

--- src/Thisweek.js
+++ src/Thisweek.js
@@ -1,9 +1,9 @@
 import Card from "./card";
 
 const cards = [
-  ["Greek salad", "Card 1 Content", "./card1.png", "Card 1 Image"],
-  ["Bruchetta", "Card 2 Content", "./card2.png", "Card 2 Image"],
-  ["Lemon Dessert", "Card 3 Content", "./card3.png", "Card 3 Image"],
+  ["Greek salad", "Card 1 Content", "./card1.png", "Card 1 Image","$12.99"],
+  ["Bruchetta", "Card 2 Content", "./card2.png", "Card 2 Image","$5.99"],
+  ["Lemon Dessert", "Card 3 Content", "./card3.png", "Card 3 Image","$5.00"],
 ];
 function ThisWeek() {
     return (
@@ -20,6 +20,7 @@ function ThisWeek() {
           content={card[1]} 
           imagePath={card[2]} 
           imgalt={card[3]}
+          price={card[4]}
         />
       ))}
         </div>
--- src/card.js
+++ src/card.js
@@ -2,9 +2,12 @@ import { getByPlaceholderText } from "@testing-library/react";
 
 function Card(props) {
     return (
-      <div>
+      <div className="card">
         <img src={props.imgepath} alt={props.imgalt}></img>
-        <h2>{props.title}</h2>
+        <div className="card-header">
+        <h4>{props.title}</h4>
+        <p>{props.price}</p>
+        </div>
         <p>{props.content} </p>
         <button>button</button>
       </div>
