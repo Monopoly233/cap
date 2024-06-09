8a5604be521145f3f31506b662295fe0f235b847
Monopoly233 <lslsooke17@gmail.com>
2024-06-08 20:14:26 -0700

    chagne

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ContactPage() {
   return (
     <div>
-      <h1>ContactPage Page</h1>
+      <h1>ContactPage</h1>
       {/* Add your home page content here */}
     </div>
   );
